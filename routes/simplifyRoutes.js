ddef2205f3aa3f30a5b8aafb10f9d1e41d16e096
Superdaadi <dadimanuelsch@gmail.com>
2026-01-18 03:13:50 +0100

    Finished and rewrote backend for text input

--- routes/simplifyRoutes.js
+++ routes/simplifyRoutes.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { generateAIResponse, generateSimplifyResponse } from '../controllers/simplifyController.js';
+import { generateSimplifyResponse } from '../controllers/simplifyController.js';
 
 const router = express.Router();
 
 
-router.post('/generateAIResponse', generateAIResponse);
 router.post('/generateSimplifyResponse', generateSimplifyResponse);
 
 
